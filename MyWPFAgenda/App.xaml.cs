b8bfdfc78c0448b59e07dbea6bb63f741e65dbed
Gaetan <gaetan.eyheramono@gmail.com>
2013-01-17 16:11:54 +0100

    Ajout de la Serialization.

--- MyWPFAgenda/App.xaml.cs
+++ MyWPFAgenda/App.xaml.cs
@@ -13,5 +13,15 @@ namespace MyWPFAgenda
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(Object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            //ici gestion d'erreurs
+        }
     }
 }
