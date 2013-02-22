225bdaa75248095a02841d39f83c6f07af505215
Thomas Marques <marquesthom@gmail.com>
2013-02-22 13:47:10 +0100

    Tests unitaires BusinessLayer...
    Signed-off-by: Thomas Marques <marquesthom@gmail.com>

---
+++ BusinessLayerTest.UnitTest/BusinessManagerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography;
+using System.Text;
+using BusinessLayer;
+
+namespace BusinessLayerTest.UnitTest
+{
+    [TestClass]
+    public class BusinessManagerTest
+    {
+        /// <summary>
+        /// Pour dire qu'on a fait un test.
+        /// </summary>
+        [TestMethod]
+        public void Test_SHA1()
+        {
+            String text = "ma chaine à hasher en SHA 1.";
+            String encode = Encoding.UTF8.GetString(new SHA1CryptoServiceProvider().ComputeHash(Encoding.UTF8.GetBytes(text)));
+
+            Assert.AreEqual(encode,BusinessManager.CalculateSHA1(text));
+        }
+    }
+}
