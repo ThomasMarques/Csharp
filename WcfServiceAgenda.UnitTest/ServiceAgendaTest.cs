e2bfe592d2c02237ac91c28a451e14f1df43de56
Gaetan <gaetan.eyheramono@gmail.com>
2013-02-22 13:39:42 +0100

    Add unit test webservice
    Signed-off-by: Gaetan <gaetan.eyheramono@gmail.com>

---
+++ WcfServiceAgenda.UnitTest/ServiceAgendaTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfServiceAgenda;
+using System.Collections.Generic;
+
+namespace WcfServiceAgenda.UnitTest
+{
+    [TestClass]
+    public class ServiceAgendaTest
+    {
+
+        [TestMethod]
+        public void GetDataTest()
+        {
+            ServiceAgenda.ServiceAgendaClient service = new ServiceAgenda.ServiceAgendaClient();
+            string result = service.GetData(5);
+            Assert.AreEqual("You entered: 5", result);
+        }
+
+        [TestMethod]
+        public void GetAllArtistesTest()
+        {
+            ServiceAgenda.ServiceAgendaClient service = new ServiceAgenda.ServiceAgendaClient();
+            var tmp=service.GetAllArtistes();
+            Assert.AreNotEqual(tmp.Count(),0);
+        }
+
+        [TestMethod]
+        public void GetAllEventsTest()
+        {
+            ServiceAgenda.ServiceAgendaClient service = new ServiceAgenda.ServiceAgendaClient(); ;
+            Assert.AreNotEqual(service.GetAllEvents().Count(), 0);
+        }
+
+        [TestMethod]
+        public void GetAllLieuxTest()
+        {
+            ServiceAgenda.ServiceAgendaClient service = new ServiceAgenda.ServiceAgendaClient(); ;
+            Assert.AreNotEqual(service.GetAllLieux().Count(), 0);
+        }
+
+        [TestMethod]
+        public void GetAllPlanningElementsTest()
+        {
+            ServiceAgenda.ServiceAgendaClient service = new ServiceAgenda.ServiceAgendaClient(); ;
+            Assert.AreNotEqual(service.GetAllPlanningElements().Count(), 0);
+        }
+
+
+        [TestMethod]
+        public void GetAllUsersTest()
+        {
+            ServiceAgenda.ServiceAgendaClient service = new ServiceAgenda.ServiceAgendaClient(); ;
+            Assert.AreNotEqual(service.GetAllUsers().Count(), 0);
+        }
+
+
+    }
+}
